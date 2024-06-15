3ddeb249046d4fb425a919dcad42b9dabd49c041
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-06-15 14:27:09 -0300

    fix all svg React types

--- components/icons/InsuranceIcon.tsx
+++ components/icons/InsuranceIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const InsuranceIcon = (props) => (
+const InsuranceIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="42"
