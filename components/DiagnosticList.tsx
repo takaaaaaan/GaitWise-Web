bf302adf95a17d24310d8e89372965547381d366
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-06-15 12:19:33 -0300

    add DiagnosticList

---
+++ components/DiagnosticList.tsx
@@ -0,0 +1,35 @@
+import { diagnosticTableMockData } from "./utils/diagnosticTableMockData";
+
+const DiagnosticList = () => {
+    return (        
+        <section role="list" className="rounded-3xl p-5 bg-white divide-y divide-gray-100">
+            <div className="flex items-center pb-10">
+                <h2 className="text-2xl font-medium" >Diagnostic List</h2>
+            </div> 
+            <div className="flex">
+                <table className="w-full table-fixed">
+                    <thead>
+                        <tr>
+                        <th className="bg-gray-100 font-medium rounded-l-3xl text-left p-4">Problem/Diagnosis</th>
+                        <th className="bg-gray-100 font-medium text-left p-4">Description</th>
+                        <th className="bg-gray-100 font-medium rounded-r-3xl text-left p-4">Status</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                    
+                    {diagnosticTableMockData.map( (diagnostic, index) => {
+                    return (
+                    <tr key={index}>
+                       <td className="py-4 pl-4 font-light">{diagnostic.name})</td>
+                       <td className="py-4 pl-4 font-light">{diagnostic.description}</td>
+                       <td className="py-4 pl-4 font-light">{diagnostic.status}</td>
+                    </tr>
+                  )})}
+
+                </tbody>
+                </table>
+            </div>
+        </section>
+)}
+
+export default DiagnosticList
