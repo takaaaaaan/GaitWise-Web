a21335f7d80fc215435e63298dc3fd0709e2350a
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-06-15 14:13:07 -0300

    add LabResultList

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@ import PatientList from "@/components/PatientList";
 import DiagnosisHistory from '@/components/DiagnosisHistory';
 import DiagnosticList from "@/components/DiagnosticList";
 import PatientProfile from "@/components/PatientProfile";
+import LabResultsList from "@/components/LabResultList";
 
 export default function Home() {
   return (
     <main className="sm:grid sm:flex-wrap md:grid md:grid-rows-1 md:grid-flow-col md:gap-x-8 md:grid-cols-4 min-h-screen mx-4">
       <section className="mb-8 lg:mb-0" > <PatientList/> </section>
-      <section className="mb-8 lg:mb-0 col-start-2 col-end-4 grid gap-8">
+      <section className="mb-8 lg:mb-0 grid grid-cols-1 col-start-2 col-end-4 gap-8">
         <DiagnosisHistory />
         <DiagnosticList />
       </section>
-      <section>
+      <section className="mb-8 lg:mb-0 grid grid-cols-1 gap-8">
         <PatientProfile />
+        <LabResultsList />
       </section>
     </main>
   );
