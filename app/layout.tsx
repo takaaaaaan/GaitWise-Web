1b53da916cb7a65643882a9ebd7d8e7ac4509cc1
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-06-16 17:18:52 -0300

    fix navbar mobile icon and desktop layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+
 import "./globals.css";
-import NavbarTest from "@/components/NavbarTest";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NavbarTest />
+      <Navbar />
         {children}
       </body>
     </html>
