48f094e6f74a66d077846c38cae5d37eb252aca6
Eulier Gonzalez <eulier.gonzalez@bisonok.com>
2024-06-17 17:02:21 -0300

    setup react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 
 import "./globals.css";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <Navbar />
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
