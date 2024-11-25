252df346f90896d4fb769b2caa2c367d96d46a09
Ueno Gohong <taka20030902@gmail.com>
2024-11-25 17:12:03 +0900

    fix🔧 : 아이콘 링크 경로 수정 및 쿼리 문자열 형식 개선

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="public/svg/gaitwise-logo.svg=v1.0" />
+        <link rel="icon" href="/svg/gaitwise-logo.svg?v=1.0" />
       </head>
       <body className={inter.className}>
         <Providers>
